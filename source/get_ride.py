6db80ffea90e6b7d4de581ffb7d5747f3fe111ed
svissers <stijn.vissers@student.uantwerpen.be>
2018-03-01 20:32:40 +0100

    first improvement

--- source/get_ride.py
+++ source/get_ride.py
@@ -7,13 +7,23 @@ def get_ride(vehicle, ride_list, current_time):
     if ride_list == []:
         return None
     distances = {}
-
+    distances_on_time = {}
+    min_distance = manhattan(vehicle.location, ride_list[0].start_loc)
+    min_distance_index = ride_list[0]
+    min_distance_on_time = manhattan(vehicle.location, ride_list[0].start_loc)
+    min_distance_index_on_time = ride_list[0]
     for ride in ride_list:
         distance_to_start = manhattan(vehicle.location, ride.start_loc)
         distances[ride] = distance_to_start
-        if current_time + distance_to_start == ride.start_earliest:
-            return ride
-            # return min(distances, key=distances.get)
-    # print ride_list
-    # print distances
-    return min(distances, key=distances.get)
+
+        if min_distance > distance_to_start:
+            min_distance = distance_to_start
+            min_distance_index = ride
+        if current_time + distance_to_start <= ride.start_earliest:
+            if min_distance_on_time > distance_to_start:
+                min_distance_on_time = distance_to_start
+                min_distance_index_on_time = ride
+
+    if min_distance_on_time != None:
+        return min_distance_index_on_time
+    return distances[min_distance_index]
