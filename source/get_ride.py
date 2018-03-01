953f48e52174908cf9cbbab1ea78e54f55087d34
svissers <stijn.vissers@student.uantwerpen.be>
2018-03-01 20:03:02 +0100

    first working thingy

--- source/get_ride.py
+++ source/get_ride.py
@@ -4,12 +4,16 @@ def manhattan(loc1,loc2):
 def get_ride(vehicle, ride_list, current_time):
     #ride list is sorted based on starting times
     #check if you can get at a starting coord on time
+    if ride_list == []:
+        return None
     distances = {}
 
     for ride in ride_list:
-        distance_to_start = manhattan(vehicle.location, ride.location)
+        distance_to_start = manhattan(vehicle.location, ride.start_loc)
         distances[ride] = distance_to_start
         if current_time + distance_to_start == ride.start_earliest:
             return ride
             # return min(distances, key=distances.get)
+    # print ride_list
+    # print distances
     return min(distances, key=distances.get)
